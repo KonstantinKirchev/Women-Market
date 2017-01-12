2fd4051952207b9cd97bfb79db0cdcc0f3d1867b
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-01-12 18:04:21 +0200

    Auth completed

---
+++ src/app/shared/security/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AuthProviders, AuthMethods, FirebaseAuth, FirebaseAuthState } from 'angularfire2';
+
+
+@Injectable()
+export class AuthService {
+  private authState: FirebaseAuthState = null;
+
+  constructor(public auth$: FirebaseAuth) {
+    auth$.subscribe((state: FirebaseAuthState) => {
+      this.authState = state;
+    });
+  }
+
+  get authenticated(): boolean {
+    return this.authState !== null;
+  }
+
+  get id(): string {
+    return this.authenticated ? this.authState.uid : '';
+  }
+
+  signIn(provider: number): firebase.Promise<FirebaseAuthState> {
+    return this.auth$.login({provider})
+      .catch(error => console.log('ERROR @ AuthService#signIn() :', error));
+  }
+
+  signInWithPassword(): firebase.Promise<FirebaseAuthState> {
+    return this.signIn(AuthProviders.Password);
+  }
+
+  signInWithGoogle(): firebase.Promise<FirebaseAuthState> {
+    return this.signIn(AuthProviders.Google);
+  }
+
+  signOut(): void {
+    this.auth$.logout();
+  }
+}
