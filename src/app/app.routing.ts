861cc97298a47296cf5abf106aa1a751b3100704
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-01-12 18:08:38 +0200

    Routing and index work in progress ...

---
+++ src/app/app.routing.ts
@@ -0,0 +1,36 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthGuard} from "./shared/security/auth.guard";
+
+import {
+    AppComponent,
+    HomeComponent
+} from './index';
+
+const appRoutes: Routes= [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    // {
+    //     path:'login',
+    //     component: LoginComponent
+    // },
+    // {
+    //     path:'register',
+    //     component: RegisterComponent
+    // },
+    // {
+    //     path:'profile',
+    //     component: ProfileComponent,
+    //     canActivate: [AuthGuard]
+    // },
+    {
+        path:'**',
+        redirectTo: ''
+    }
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export { AppComponent } from './app.component'
+export { HomeComponent } from './home/home.component'
