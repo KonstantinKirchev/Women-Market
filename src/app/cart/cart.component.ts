9e98dfd3bb51ee0846f32cb19263ec608e5d415d
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-01-16 21:18:01 +0200

    Add products to cart. Work in progress...

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  productList: any
+  baseproductList: any
+  total: number = 0
+
   constructor() { }
 
   ngOnInit() {
+    this.productList = JSON.parse(localStorage.getItem('live-cart'))
+    for(let product of this.productList){
+      //let price = product.price as number
+      //console.log(typeof(product.price))
+      //console.log((product.price))
+      this.total = this.total + product.price
+    }
+    console.log(this.total)
+  }
+
+  decrease(i){
+    if(this.productList[i].quantity !== 1){
+      this.productList[i].quantity -= 1
+    }
+    this.baseproductList = JSON.parse(localStorage.getItem('live-cart'))
+    this.productList[i].price -= this.baseproductList[i].price
+  }
+
+  increase(i){
+    this.baseproductList = JSON.parse(localStorage.getItem('live-cart'))
+    this.productList[i].quantity += 1
+    this.productList[i].price += this.baseproductList[i].price
   }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,11 +8,24 @@ import { ProductsService } from "../shared/services/products.service";
 })
 export class ProductsComponent implements OnInit {
 
+  cart: any = []
+
   products: any
   
   constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
     this.productsService.findAllProducts().subscribe((res)=> this.products = res)
+  }
+
+  addToCart(product){
+    if(this.cart.indexOf(product) === -1){
+      this.cart.push(product)
+    }
+    // else{
+    //   product.quantity += 1
+    // }
+    localStorage.setItem('live-cart', JSON.stringify(this.cart))
+    console.log(this.cart)
   }
 }
