e6c9a1d00940a66eebcf9c0fd62513a86edc9e39
Konstantin Kirchev <konstantin1230@yahoo.com>
2017-01-21 19:30:24 +0200

    Store editing and deleting is working perfect now.

--- src/app/stores/stores.component.ts
+++ src/app/stores/stores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { StoresService } from "../shared/services/stores.service"
+import { ProductsService } from "../shared/services/products.service"
 import { AuthService } from '../shared/security/auth.service'
 import { Router } from "@angular/router"
 import { NotificationsService } from "angular2-notifications"
@@ -12,8 +13,10 @@ import { NotificationsService } from "angular2-notifications"
 export class StoresComponent implements OnInit {
 
   stores: any
+  products: any
 
   constructor(private storesService: StoresService, 
+              private productsService: ProductsService,
               private auth: AuthService,
               private router: Router,
               private _service: NotificationsService) { }
@@ -26,10 +29,15 @@ export class StoresComponent implements OnInit {
     this.router.navigate(['/stores/edit/' + store.$key])
   }
 
-  deleteStore(key){
-    this.storesService.deleteStore(key)
-    .subscribe(
-      ()=>this._service.success(
+  deleteStore(store){
+    this.productsService.findAllProducts()
+    .subscribe((res)=>{
+      this.products = res
+      for(let product of this.products){
+        if(store.name == product.store){
+          this.productsService.deleteProduct(product.$key)
+              .subscribe(()=>{
+                this._service.success(
                             'The product was deleted successfully',
                             'Continue editing/deleting',
                             {
@@ -37,10 +45,29 @@ export class StoresComponent implements OnInit {
                                 showProgressBar: true,
                                 pauseOnHover: false,
                                 clickToClose: true
+                })
+              })
+        } 
+      }
+    })
+    
+    this.storesService.deleteStore(store.$key)
+    .subscribe(
+      ()=>
+      {
+        this._service.success(
+                            'The store was deleted successfully',
+                            'Continue editing/deleting',
+                            {
+                                timeOut: 3000,
+                                showProgressBar: true,
+                                pauseOnHover: false,
+                                clickToClose: true
                             }
-                        ),
+      )
+    },
         err => this._service.error(
-                            'This product cannot be deleted',
+                            'This store cannot be deleted',
                              err,
                             {
                                 timeOut: 3000,
@@ -48,8 +75,7 @@ export class StoresComponent implements OnInit {
                                 pauseOnHover: false,
                                 clickToClose: true
                             }
-                        ),
-          () => console.log('Finished'));
-  }
+                        ));
+   }
 
 }
